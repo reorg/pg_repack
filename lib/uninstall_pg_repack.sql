9528811fe90ac7c10af274e32afe398f7e8b0bfd
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2012-11-11 03:00:00 +0000

    Added The Reorg Development Team copyright

--- lib/uninstall_pg_repack.sql
+++ lib/uninstall_pg_repack.sql
@@ -3,6 +3,7 @@
  *
  * Portions Copyright (c) 2008-2011, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
  * Portions Copyright (c) 2011, Itagaki Takahiro
+ * Portions Copyright (c) 2012, The Reorg Development Team
  */
 
 DROP SCHEMA IF EXISTS repack CASCADE;
