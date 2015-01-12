080ebd3ed697be116f8aff66ce2a1e8af97192c6
Josh Kupershmidt <schmiddy@gmail.com>
2015-01-11 19:01:37 -0500

    Bump up Copyright notices for 2015

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@
 #
 #  Portions Copyright (c) 2008-2011, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
 #  Portions Copyright (c) 2011, Itagaki Takahiro
-#  Portions Copyright (c) 2012, The Reorg Development Team
+#  Portions Copyright (c) 2012-2015, The Reorg Development Team
 #
 
 PG_CONFIG ?= pg_config
--- lib/pgut/pgut-be.c
+++ lib/pgut/pgut-be.c
@@ -3,7 +3,7 @@
  * pgut-be.c
  *
  * Copyright (c) 2009-2011, NIPPON TELEGRAPH AND TELEPHONE CORPORATION
- * Portions Copyright (c) 2012, The Reorg Development Team
+ * Portions Copyright (c) 2012-2015, The Reorg Development Team
  *
  *-------------------------------------------------------------------------
  */
